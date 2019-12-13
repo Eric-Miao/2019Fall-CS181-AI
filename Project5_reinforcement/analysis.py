206754e2fd53902738c5d749793ff72013a0ae63
Eric Miao <miaoyx@shanghaitech.edu.cn>
2019-12-14 01:00:12 +0800

    q8 unfinished

--- Project5_reinforcement/analysis.py
+++ Project5_reinforcement/analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -25,10 +25,12 @@ Noise: determines whether u want to avoid the risk, negative states
 LivingCost: how fast u want to end the game, no matter die or win 
 """
 
+
 def question2():
     answerDiscount = 0.9
     answerNoise = 0.0
     return answerDiscount, answerNoise
+
 
 def question3a():
     answerDiscount = 0.5
@@ -37,12 +39,14 @@ def question3a():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3b():
     answerDiscount = 0.5
     answerNoise = 0.3
     answerLivingReward = -3
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3c():
     answerDiscount = 0.9
@@ -51,12 +55,14 @@ def question3c():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3d():
     answerDiscount = 0.9
     answerNoise = 0.3
     answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3e():
     answerDiscount = 0.0
@@ -65,15 +71,17 @@ def question3e():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question8():
-    answerEpsilon = None
-    answerLearningRate = None
+    answerEpsilon = 0.9
+    answerLearningRate = 0.6
     return answerEpsilon, answerLearningRate
     # If not possible, return 'NOT POSSIBLE'
 
+
 if __name__ == '__main__':
-    print ('Answers to analysis questions:')
+    print('Answers to analysis questions:')
     import analysis
     for q in [q for q in dir(analysis) if q.startswith('question')]:
         response = getattr(analysis, q)()
-        print ('  Question %s:\t%s' % (q, str(response)))
+        print('  Question %s:\t%s' % (q, str(response)))
